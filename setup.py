c7859201c43602191ac27640b3c9834ed685cffb
Andrew Yu <andrewthomasyu@gmail.com>
2024-12-02 23:49:18 +0800

    Refactor setup.py to remove deprecated libraries

--- setup.py
+++ setup.py
@@ -1,29 +1,32 @@
+from setuptools import setup
+from pathlib import Path
 
-from distutils.core import setup
 setup(
-  name = 'scipy-optimstruct',         
-  packages = ['src/scipy_optimstruct'],   
-  version = '0.1.1',     
-  license='MIT',        
-  description = 'Enable easier organization of variables and constraints for Scipy Optimize',   
-  author = 'Andrew Yu',                  
-  author_email = 'andrewthomasyu@gmail.com',
-  url = 'https://github.com/andrewnyu/scipy-optimstruct',   
-  download_url = 'https://github.com/andrewnyu/scipy-optimstruct/archive/refs/tags/v0.1.tar.gz',    
-  keywords = ['scipy', 'optimization', 'data structures'],   
-  install_requires=[           
-          'numpy',
-          'scipy',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      
-    'Intended Audience :: Developers',      
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',  
-    'Programming Language :: Python :: 3', 
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.8',
-  ],
+    name='scipy-optimstruct',
+    packages=['scipy_optimstruct'],  # Adjusted
+    package_dir={'': 'src'},         # Added
+    version='0.1.1',
+    license='MIT',
+    description='Enable easier organization of variables and constraints for Scipy Optimize',
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    author='Andrew Yu',
+    author_email='andrewthomasyu@gmail.com',
+    url='https://github.com/andrewnyu/scipy-optimstruct',
+    download_url='https://github.com/andrewnyu/scipy-optimstruct/archive/refs/tags/v0.1.tar.gz',
+    keywords=['scipy', 'optimization', 'data structures'],
+    install_requires=[
+        'numpy',
+        'scipy',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+    ],
+    python_requires='>=3.6',  # Recommended to specify supported Python versions
 )
